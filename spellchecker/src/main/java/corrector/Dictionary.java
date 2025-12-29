64cb7ea1a716870b0e1a840bb1fee695e1fc11f3
VictoriaDibar <133808746+VictoriaDibar@users.noreply.github.com>
2025-12-29 16:24:55 -0300

    Add files via upload

---
+++ spellchecker/src/main/java/corrector/Dictionary.java
@@ -0,0 +1,10 @@
+package corrector;
+
+import java.io.IOException;
+import java.util.Set;
+
+public interface Dictionary {
+    int getNumWords();
+    boolean isWord(String word);
+    Set<String> getWords();
+}
